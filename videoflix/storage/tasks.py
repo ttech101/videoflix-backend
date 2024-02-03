30c6225ead1e2084a419eaadf40d8ed5fafeaf6b
Tony <ttech@gmx.de>
2024-02-03 10:11:52 +0100

    update

--- videoflix/storage/tasks.py
+++ videoflix/storage/tasks.py
@@ -12,6 +12,7 @@ def convert_video(source,instance):
     print('video wwird nun umgewandelt')
     source_name = os.path.splitext(source)[0]
     target = source_name + '_480p.mp4'
+    print('bin ich auch noch hier???')
     cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(source, target)
     subprocess.run(cmd)
     print('umwandel ist abgeschlossen')
