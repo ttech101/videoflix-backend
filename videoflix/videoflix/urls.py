5d1cd9f30da3fa69bc8517f655a2ea563a43da96
Tony <ttech@gmx.de>
2024-02-06 09:15:32 +0100

    edit urls

--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -42,8 +42,7 @@ urlpatterns = [
     path('delete_movie/', DeleteMovie.as_view(), name='delete_movie'),
     path("__debug__/", include("debug_toolbar.urls")),
     path('django-rq/', include('django_rq.urls')),
-    path('media/<path:path>', include('media.urls'), {'document_root': settings.MEDIA_ROOT}),
-    #, login_required(serve, login_url='https://videoflix.tech-mail.eu/login')
+    path('media/<path:path>', login_required(serve, login_url='https://videoflix.tech-mail.eu/login'), {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += router.urls
