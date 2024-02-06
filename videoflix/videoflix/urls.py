1d8d6b688065548ab22ab70a4a968dfb71a993bc
Tony <ttech@gmx.de>
2024-02-06 10:12:07 +0100

    edit

--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path('django-rq/', include('django_rq.urls')),
     #path('media/<path:path>', ShowMedia),
-    path('media/<path:path>', login_required(serve, login_url='https://videoflix.tech-mail.eu/login'), {'document_root': settings.MEDIA_ROOT}),
+    path('media/<path:path>', login_required(serve, login_url=''), {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
