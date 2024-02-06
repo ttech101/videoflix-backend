d729cc84dbd5ac631b94ff27e04207599fb8c212
Tony <ttech@gmx.de>
2024-02-06 11:50:27 +0100

    update

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
     }
 }
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/tt/projekte/videoflix-backend/videoflix'
 MEDIA_URL = '/media/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
