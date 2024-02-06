2a94db924be3c2a2be94d6d70ef5ff91f584e1ae
Tony <ttech@gmx.de>
2024-02-06 07:02:38 +0100

    upload

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -24,6 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(snwa47!t1=ak__*^%=ue&e%(2bp$kpd5mh8_%a83jz@trwgc-'
 
+SESSION_COOKIE_SECURE=True
+CSRF_COOKIE_SECURE=True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
