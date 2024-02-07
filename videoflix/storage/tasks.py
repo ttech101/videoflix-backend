3f8fb63b7989be61b3af2284f5a24d2ff1ae1bd0
Tony <ttech@gmx.de>
2024-02-07 10:13:37 +0100

    update all

--- videoflix/storage/tasks.py
+++ videoflix/storage/tasks.py
@@ -7,9 +7,7 @@ def convert_480p(instance):
     source_name = os.path.splitext(instance.video.path)[0]
     video_name = os.path.splitext(instance.video.name)[0]
     target = source_name + '96647835AA3E5537424ABDB439F6E_480p.mp4'
-    # cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(instance.video.path, target)
     subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-s', 'hd720', '-c:v', 'libx264', '-crf', '23', '-c:a', 'aac', '-strict', '-2',  target])
-   # subprocess.run(cmd)
     os.remove(instance.video.path)
     instance.video.name = video_name + '96647835AA3E5537424ABDB439F6E_480p.mp4'
     instance.save()
