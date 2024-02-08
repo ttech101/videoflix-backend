1573790504663274b87d0115618e00c7dfcb87d6
Tony <ttech@gmx.de>
2024-02-08 12:30:45 +0100

    edit subprocess

--- videoflix/storage/tasks.py
+++ videoflix/storage/tasks.py
@@ -16,9 +16,9 @@ def convert_480p(instance):
     source_name = os.path.splitext(instance.video.path)[0]
     video_name = os.path.splitext(instance.video.name)[0]
     target = source_name + timestamp + '_480p.mp4'
-    #subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-s', 'hd720', '-c:v', 'libx264', '-crf', '23', '-c:a', 'aac', '-strict', '-2',  target])
-    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(instance.video.path, target)
-    subprocess.run(cmd)
+    subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-s', 'hd720', '-c:v', 'libx264', '-crf', '23', '-c:a', 'aac', '-strict', '-2',  target])
+    #cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(instance.video.path, target)
+    #subprocess.run(cmd)
     os.remove(instance.video.path)
     instance.video.name = video_name + timestamp + '_480p.mp4'
 
@@ -28,9 +28,9 @@ def convert_480p(instance):
         video_path = instance.video.path.replace("\\video\\", "\\cover\\")
         video_path_without_extension = os.path.splitext(video_path)[0]
         target = video_path_without_extension  + '.png'
-        #subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-ss' , middle_time_str,'-frames:v' , '1' ,'-vf', 'scale=iw/2:ih/2' ,target])
-        cmd ='ffmpeg -i "{}" -ss "{}" -frames:v 1 -vf scale=iw/2:ih/2 "{}"'.format(instance.video.path,middle_time_str, target)
-        subprocess.run(cmd)
+        subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-ss' , middle_time_str,'-frames:v' , '1' ,'-vf', 'scale=iw/2:ih/2' ,target])
+        #cmd ='ffmpeg -i "{}" -ss "{}" -frames:v 1 -vf scale=iw/2:ih/2 "{}"'.format(instance.video.path,middle_time_str, target)
+        #subprocess.run(cmd)
         sliced_path = target.split("media\\", 1)[1]
         instance.cover.name = sliced_path
 
@@ -40,9 +40,9 @@ def convert_480p(instance):
         video_path = instance.video.path.replace("\\video\\", "\\big_picture\\")
         video_path_without_extension = os.path.splitext(video_path)[0]
         target = video_path_without_extension  +   '.png'
-        #subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-ss' , middle_time_str,'-frames:v' , '1',target])
-        cmd ='ffmpeg -i "{}" -ss "{}" -frames:v 1 "{}"'.format(instance.video.path,middle_time_str, target)
-        subprocess.run(cmd)
+        subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-ss' , middle_time_str,'-frames:v' , '1',target])
+        #cmd ='ffmpeg -i "{}" -ss "{}" -frames:v 1 "{}"'.format(instance.video.path,middle_time_str, target)
+        #subprocess.run(cmd)
         sliced_path = target.split("media\\", 1)[1]
         instance.big_picture.name = sliced_path
         instance.convert_status = 2
