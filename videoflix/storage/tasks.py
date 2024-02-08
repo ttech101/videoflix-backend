2d2adc1f9a4d2f308ef5251066cc89f9f4529df9
Tony <ttech@gmx.de>
2024-02-08 12:45:55 +0100

    add

--- videoflix/storage/tasks.py
+++ videoflix/storage/tasks.py
@@ -25,25 +25,25 @@ def convert_480p(instance):
     if instance.automatic_cover:
         middle_time = duration / 5
         middle_time_str = '{:0.3f}'.format(middle_time)
-        video_path = instance.video.path.replace("//video//", "//cover//")
+        video_path = instance.video.path.replace("/video/", "/cover/")
         video_path_without_extension = os.path.splitext(video_path)[0]
         target = video_path_without_extension  + '.png'
         subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-ss' , middle_time_str,'-frames:v' , '1' ,'-vf', 'scale=iw/2:ih/2' ,target])
         #cmd ='ffmpeg -i "{}" -ss "{}" -frames:v 1 -vf scale=iw/2:ih/2 "{}"'.format(instance.video.path,middle_time_str, target)
         #subprocess.run(cmd)
-        sliced_path = target.split("media//", 1)[1]
+        sliced_path = target.split("media/", 1)
         instance.cover.name = sliced_path
 
     if instance.automatic_image:
         middle_time = duration / 2
         middle_time_str = '{:0.3f}'.format(middle_time)
-        video_path = instance.video.path.replace("//video//", "//big_picture//")
+        video_path = instance.video.path.replace("/video/", "/big_picture/")
         video_path_without_extension = os.path.splitext(video_path)[0]
         target = video_path_without_extension  +   '.png'
         subprocess.run(['/usr/bin/ffmpeg', '-i', instance.video.path, '-ss' , middle_time_str,'-frames:v' , '1',target])
         #cmd ='ffmpeg -i "{}" -ss "{}" -frames:v 1 "{}"'.format(instance.video.path,middle_time_str, target)
         #subprocess.run(cmd)
-        sliced_path = target.split("media//", 1)[1]
+        sliced_path = target.split("media/", 1)
         instance.big_picture.name = sliced_path
         instance.convert_status = 2
 
